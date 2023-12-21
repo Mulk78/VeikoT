9c6b4b9d2252e14796a1dab684cdd08185e094ff
Mulk78 <120668827+Mulk78@users.noreply.github.com>
2023-12-21 16:09:33 +0200

    WP3 veebipood

---
+++ W3/ws3/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg'; (kaldkriipsud tõstavad selle rea koodist välja)
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="pilt" src="https://cdn.motor1.com/images/mgl/2b79K/s3/official-honda-africa-twin-specs-announced-.jpg" alt="" />
+      <button className="nupu-stiil">Vajuta siia</button>
+    </div>
+  );
+}
+
+export default App;
