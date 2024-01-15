f28a8bc7c968b231f930c54695e6e8e452b56f1d
Mulk78 <120668827+Mulk78@users.noreply.github.com>
2024-01-15 15:20:42 +0200

    3. tund

--- W3/ws3/src/pages/Avaleht.js
+++ W3/ws3/src/pages/Avaleht.js
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Avaleht() {
+  const [kogus, uuendaKogus] = useState (0);
+  const [laigitud, muudaLaigitud] = useState (false);
+  const [sonum, uuendaSonum] = useState ("Uuenda kogust!");
+  const nulli = () => {
+    uuendaKogus(0);
+    uuendaSonum("Nullitud!");
+  }
+  const vahenda = () => {
+    uuendaKogus( kogus -1 );
+    uuendaSonum("Vähendatud!");
+  }
+  const suurenda = () => {
+    uuendaKogus(kogus +1 );
+    uuendaSonum("Suurendatud!");
+  }
+
   return (
     <div>
-      <img src="/syda mitteliketud.svg" alt="" />
-      <img src="/syda liketud.svg" alt="" />
-      <img src="/syda kuldne.svg" alt="" />
-      <button className="nupu-stiil">Pane liketuks</button>
-      <button className="nupu-stiil">Pane mitteliketud</button>
+      <div> {sonum} </div>
+      {laigitud === false && <img src="/syda mitteliketud.svg" alt="" />}
+      {laigitud === true && <img src="/syda liketud.svg" alt="" />}
+      {kogus > 10 && <img src="/syda kuldne.svg" alt="" />}
+      <button className="nupu-stiil" onClick={()=>muudaLaigitud(true)} >Pane liketuks</button>
+      <button className="nupu-stiil" onClick={()=>muudaLaigitud(false)} >Pane mitteliketud</button>
 
       <br /><br />
-      
-      <button className="nupu-stiil"> - </button>
-      <span> 7 </span>
-      <button className="nupu-stiil"> + </button>
+      {kogus !== 0 && <button className="nupu-stiil" onClick={nulli} > Nulli </button>}
+      <br />
+      <button disabled = {kogus === 0} className="nupu-stiil" onClick={vahenda} > - </button>
+      <span> {kogus} </span>
+      <button className="nupu-stiil" onClick={suurenda} > + </button>
     </div>
   )
 }
--- W3/ws3/src/pages/LisaToode.js
+++ W3/ws3/src/pages/LisaToode.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 function LisaToode() {
+  const [sonum, m22raSonum] = useState ("Lisa toode");
+  const inputiLuger = useRef();
+
+  function lisa() {
+    if (inputiLuger.current.value === "") {
+      m22raSonum ("Tühja nimetusega ei saa toodet lisada!");
+    } else {
+      m22raSonum ("Toode lisatud : " + inputiLuger.current.value);
+    }
+   }
+
   return (
     <div>
+      <div> {sonum} </div>
       <label>Uus toode</label><br />
-      <input type="text" /><br />
-      <button className="nupu-stiil">Sisesta</button><br />
+      <input ref={inputiLuger} type="text" /><br />
+      <button onClick={lisa} className="nupu-stiil">Sisesta</button><br />
     </div>
   )
 }
